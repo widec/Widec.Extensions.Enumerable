a618c9eca25221d4f5d2e039a0251f2aa2901ea4
Wim De Cleen <wim@widec.be>
2016-09-15 20:42:26 +0200

    Add project files.

---
+++ src/Widec.Extensions.Enumerable/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Widec.Extensions.Enumerable
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
