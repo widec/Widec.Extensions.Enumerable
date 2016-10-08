41f57fb4f701ba34902a1f1da61389c2da3e2a32
Wim De Cleen <wim>
2016-10-08 22:02:16 +0200

    next version

--- src/Widec.Extensions.Enumerable/Enumerable.cs
+++ src/Widec.Extensions.Enumerable/Enumerable.cs
@@ -21,9 +21,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Widec.Extensions.Enumerable
 {
